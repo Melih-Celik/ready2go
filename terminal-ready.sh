df2e9a9ed42f5826125a7bd1216b677acb008725
Melih <celikmelih54@gmail.com>
2024-12-21 21:11:35 +0300

    Update terminal-ready.sh

--- terminal-ready.sh
+++ terminal-ready.sh
@@ -2,5 +2,5 @@ apt install -y zsh unzip
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" 
 wget https://raw.githubusercontent.com/Melih-Celik/terminal-ready/refs/heads/main/zsh_settings.zip
 unzip -o zsh_settings.zip -d $HOME
-exec zsh
 chsh -s /bin/zsh $USER
+exec zsh
