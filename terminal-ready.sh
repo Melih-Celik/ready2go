4a270ee3526e204a9bf6eba433688fde0d17e873
Melih <celikmelih54@gmail.com>
2024-12-21 20:05:44 +0300

    Update terminal-ready.sh

--- terminal-ready.sh
+++ terminal-ready.sh
@@ -1,3 +1,4 @@
 apt install -y zsh unzip
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-unzip zsh_settings.zip -d $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+unzip -o $HOME/terminal-ready/zsh_settings.zip -d $HOME
+source $HOME/.zshrc
